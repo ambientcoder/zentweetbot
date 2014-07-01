ec5e3e6fd6b0e44743eb7ccabd63470cd0ce7881
Stephen Scott <sscott@ozu.local>
2014-07-01 12:14:56 +1000

    set rand_limit to 8 in variables.rb and dispplay parameters when running

--- variables.rb
+++ variables.rb
@@ -1,9 +1,9 @@
 # run 1 out of every $rand_limit times roughly (default is 10)
-#$rand_limit = 5
+$rand_limit = 8
+#
+# (default is 2, 1 is more incoherent, 3 is more lucid)
+#$markov_index = 1
 #
 # defaults are false
 #$include_urls = true
 #$include_replies = true
-#
-# (default is 2, 1 is more incoherent, 3 is more lucid)
-#$markov_index = 1
